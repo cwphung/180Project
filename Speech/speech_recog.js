c1170e6e7f7cbe8d84717179d841c48eed2b328c
nturner0 <nturner1@g.ucla.edu>
2024-04-17 14:31:37 -0700

    Updated speech recognition JS

--- Speech/speech_recog.js
+++ Speech/speech_recog.js
@@ -1,5 +1,43 @@
+//TODO:
+//Grant microphone permission permanently after one press: happens on laptop, but not desktop browser
+//Cut off speech after x seconds, and still report results
+//Convert the input parsing from Python to JS - DONE
+//Re-add error handling wrapper if Speech Recognition is not available
+//If no result, need to handle it
+
+function response_parser(word_list, keywords) {
+    const count = [];
+    for (let i=0; i<word_list.length; i++) {
+        for (let j=0; j<keywords.length; j++) {
+            count[j] = 0;
+            for (let k=0; k<keywords[j].length; k++) {
+                if (word_list[i].includes(keywords[j][k]))
+                    count[j]++;
+            }
+        }
+    }
+
+    let temp_max = 0;
+    let temp_max_i = 0;
+    for (let i = 0; i<count.length; i++) {
+        if (count[i] > temp_max) {
+            temp_max = count[i];
+            temp_max_i = i;
+        }
+    }
+
+    if (temp_max == 0)
+        return "No keyword detected";
+
+    return keywords[temp_max_i][0];
+};
+
 // Create a new instance of SpeechRecognition
 recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+//recognition.continuous = true;
+recognition.onnomatch = function(event) {
+    console.log('No speech was recognized.');
+  };
 
 // Define the recognition result event handler
 recognition.onresult = function(event) {
@@ -8,9 +46,18 @@ recognition.onresult = function(event) {
 
     // Get the recognized text from the result
     text = result[0].transcript;
+    word_list = text.split(" ")
+
+    const keywords = [["right", "riot", "write"], ["left"], ["up", "pop"], ["down"], ["enter"], ["back"]];
+
+    input_move = response_parser(word_list, keywords);
 
     // Display the recognized text in the result element
-    document.getElementById('result').textContent = text;
+    document.getElementById('result').textContent = input_move;
+};
+
+function nomatch() {
+    console.log("no match detected")
 };
 
 // Define the recognition error event handler
@@ -24,9 +71,9 @@ function startRecognition() {
     recognition.start();
 
     //doesnt work currently
-    setTimeout(function() {
-        recognition.stop();
-      }, 5000);
+    // setTimeout(function() {
+    //     recognition.stop();
+    //   }, 2000);
 }
   
   // Add event listener to the button
